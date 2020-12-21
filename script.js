4dff0f0a36fc974907f57e4ec126bfd8301266b7
Jack Stag <uv.mhjl@gmail.com>
2020-12-20 23:36:45 -0500

    update

--- script.js
+++ script.js
@@ -9,13 +9,23 @@ function jump() {
         char.classList.remove("animate");
     },500);
 }
+var seconds = 0;
+var score = document.getElementById("score");
 
-// var dead = setInterval(function(){
-//     var charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"));
-//     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-//     if (blockLeft < 20 && blockLeft > 0 && charTop >= 130){
-//         block.style.animation = "none";
-//         block.style.display = "none";
-//         alert("u lose");
-//     }
-// }, 10);
+function incrementSeconds() {
+    seconds += 1;
+    score.innerText = "Score " + seconds;
+}
+
+var cancel = setInterval(incrementSeconds, 1000);
+
+
+var dead = setInterval(function(){
+    var charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 20 && blockLeft > 0 && charTop >= 130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("u lose " + "your score was " + seconds);
+    }
+}, 10);
